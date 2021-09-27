d1526c93f20604d004d4105a0feb24bad18b2e75
kcheykim <kosalcheykim@gmail.com>
2021-09-27 14:01:26 -0700

    create a working fight function

---
+++ assets/js/game.js
@@ -0,0 +1,45 @@
+//prompting interaction from the user
+var playerName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+// You can also log multiple values at once like this 
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(
+        playerName + " attacked " + enemyName + ". " + 
+        enemyName + " now has " + enemyHealth + " health remaining.");
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(
+        enemyName + " attacked " + playerName + ". " + 
+        playerName + " now has " + playerHealth + " health remaining.");
+};
+
+/*FUNCTION EXPRESSION*/
+
+fight();
+
+/*FUNCTION DECL
+function fight() {
+    window.alert("The fight has begun!");
+} */
+
+/*
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName); */
+
+//This pass a message that JavaScript is running
+//window.alert("This is an alert! JavaScript is running!");
+//calling the function
+//fight();
