8074befa19ba55503521379a2bc9d734817182dd
kcheykim <kosalcheykim@gmail.com>
2021-09-30 10:40:38 -0700

    fixed null playerName bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,9 +4,17 @@ var randomNumber = function(min, max) {
   
     return value;
 };
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("what is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -36,7 +44,14 @@ var playerInfo = {
         }
       }
 };
-
+var getPlayerName = function() {
+  var name = "";
+  while (name === "" || name === null) {
+    name = prompt("what is your robot's name?");
+  }
+  console.log("Your robot's name is " + name);
+  return name;
+};
 /*
 //prompting interaction from the user
 var playerInfo.name = window.prompt("what is your robot's name?");
